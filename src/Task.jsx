64e055a82865e992fdeafd15218c49db6b83b505
David Boland <dboland77@gmail.com>
2021-03-23 11:34:18 +0000

    Tidy up and rename

--- src/Task.jsx
+++ src/Task.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 
-const divStyle = {
-  margin: "10px",
-  border: "1px solid black"
-};
-
 export default class Task extends React.Component {
   render() {
     return (
@@ -15,7 +10,6 @@ export default class Task extends React.Component {
             {...provided.draggableProps}
             {...provided.dragHandleProps}
             ref={provided.innerRef}
-            //style={divStyle}
           >
             {this.props.task.content}
           </div>
